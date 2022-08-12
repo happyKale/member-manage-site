08e3930a2ddf3b13d0f7b6a7bd4d3d9f2c971504
happyKale <syk767@gmail.com>
2022-08-12 17:23:55 +0900

    Feat: react-route-dom 적용해서 화면 라우팅

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,25 @@
-function App() {
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { HomeScreen, DetailScreen, AddScreen, ModifyScreen} from './screens';
+
+function App(){
   return (
-    <div className="App"/>
+    <div className="App">
+      <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>}/>
+            <Route path="detail">
+              <Route path=":id" element={<DetailScreen/>}/>  
+            </Route>
+            <Route path="add" element={<AddScreen/>}/>
+            <Route path="modify">
+              <Route path=':id' element={<ModifyScreen/>}/>  
+            </Route>
+          </Routes>
+          </main>
+    </div>
   );
-}
+};
 
 export default App;
+
