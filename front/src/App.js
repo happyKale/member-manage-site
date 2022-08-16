7929c627ef1149398e756c3607c626daa372884a
happyKale <syk767@gmail.com>
2022-08-16 17:34:18 +0900

    Edit: App.js Container 컴포넌트 적용

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Footer, Header } from './components';
-import { HomeScreen, DetailScreen, AddScreen, ModifyScreen} from './screens';
+import { Container } from "@mui/system";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Footer, Header } from "./components";
+import { HomeScreen, DetailScreen, AddScreen, ModifyScreen } from "./screens";
 
-function App(){
+function App() {
   return (
     <div className="App">
-      <Header/>
-      <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen/>}/>
-            <Route path="detail">
-              <Route path=":id" element={<DetailScreen/>}/>  
-            </Route>
-            <Route path="add" element={<AddScreen/>}/>
-            <Route path="modify">
-              <Route path=':id' element={<ModifyScreen/>}/>  
-            </Route>
-          </Routes>
-        </main>
-        <Footer/>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="detail">
+            <Route path=":id" element={<DetailScreen />} />
+          </Route>
+          <Route path="add" element={<AddScreen />} />
+          <Route path="modify">
+            <Route path=":id" element={<ModifyScreen />} />
+          </Route>
+        </Routes>
+      </Container>
+      <Footer />
     </div>
   );
-};
+}
 
 export default App;
-
