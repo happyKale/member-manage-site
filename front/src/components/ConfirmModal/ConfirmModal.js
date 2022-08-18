402795e6f089803ac8f01a5f4f4761f24798a86d
happyKale <syk767@gmail.com>
2022-08-18 18:17:51 +0900

    Feat: 확인 모달 추가

---
+++ front/src/components/ConfirmModal/ConfirmModal.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { Box, Button, Modal } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import { modify } from "../../store/modalReducer";
+
+function ConfirmModal() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const type = location.pathname.split("/")[1];
+  const [open, setOpen] = useState(true);
+
+  const handleClose = async () => {
+    setOpen(false);
+    await dispatch(
+      modify({ type: "", openStatus: false, selectedMemberId: [] })
+    );
+    if (type === "add") {
+      navigate(`/`);
+    }
+  };
+
+  const handleConfirm = () => {
+    handleClose();
+    navigate(`/`);
+  };
+
+  const handleStay = () => {
+    const memberId = location.path.split("/").pop();
+    navigate(`/detail/${memberId}`);
+  };
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "400px",
+          height: "400px",
+          backgroundColor: "white",
+        }}
+      >
+        확인 모달
+        <Button
+          style={{
+            width: "80px",
+            height: "35px",
+            backgroundColor: "green",
+            marginRight: "15px",
+            color: "white",
+          }}
+          onClick={handleConfirm}
+        >
+          확인
+        </Button>
+        {type === "modify" && (
+          <Button
+            style={{
+              width: "80px",
+              height: "35px",
+              backgroundColor: "blue",
+              marginRight: "15px",
+              color: "white",
+            }}
+            onClick={handleStay}
+          >
+            머무르기
+          </Button>
+        )}
+      </Box>
+    </Modal>
+  );
+}
+
+export { ConfirmModal };
--- front/src/components/index.js
+++ front/src/components/index.js
@@ -1,3 +1,4 @@
 export * from "./Footer";
 export * from "./Header";
 export * from "./RemoveModal";
+export * from "./ConfirmModal";
