b378efb297d9d2416c7241e110f3f321bc9315a9
happyKale <syk767@gmail.com>
2022-08-22 13:08:40 +0900

    Feat: memberReducer load액션 리듀서 구현

--- front/src/store/memberReducer.js
+++ front/src/store/memberReducer.js
@@ -1,67 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  {
-    id: 1,
-    name: "직원1",
-    phone: "010-1234-5678",
-    birth: "1980-01-01",
-    team: "DX",
-    position: "팀장", // 직책
-    rank: "부장", // 직급
-    email: "123@naver.com",
-    officeNum: "02-2125-6530",
-    faxNum: "123456",
-    task: "업무1",
-  },
-  {
-    id: 2,
-    name: "직원2",
-    phone: "010-1234-5678",
-    birth: "1995-01-01",
-    team: "DX",
-    position: "팀원", // 직책
-    rank: "대리", // 직급
-    email: "456@naver.com",
-    officeNum: "02-2125-6547",
-    faxNum: "123456",
-    task: "업무2",
-  },
-  {
-    id: 3,
-    name: "직원3",
-    phone: "010-1234-5678",
-    birth: "1997-01-01",
-    team: "DX",
-    position: "팀원", // 직책
-    rank: "사원", // 직급
-    email: "789@naver.com",
-    officeNum: "02-2125-6548",
-    faxNum: "123456",
-    task: "업무3",
-  },
-  {
-    id: 4,
-    name: "직원4",
-    phone: "010-1234-5678",
-    birth: "1996-01-01",
-    team: "DX",
-    position: "팀원", // 직책
-    rank: "사원", // 직급
-    email: "555@naver.com",
-    officeNum: "02-2125-6549",
-    faxNum: "123456",
-    task: "업무4",
-  },
-];
+const initialState = {
+  memberList: [],
+  selectedMemberInfo: {},
+};
 
 export const memberSlice = createSlice({
   name: "memeber",
   initialState,
   reducers: {
-    load: (state, action) => {},
+    load: (state, action) => {
+      if (action.payload.memberList) {
+        state.memberList = action.payload.memberList;
+      }
+      if (action.payload.selectedMemberInfo) {
+        state.selectedMemberInfo = action.payload.selectedMemberInfo;
+      }
+      return state;
+    },
     add: (state, action) => {
-      state.push(action.payload);
+      state.memberList.push(action.payload);
       return state;
     },
     remove: (state, action) => {
