65ae9639d5dcda51a026c4fc59f5349218e18957
happyKale <syk767@gmail.com>
2022-08-22 13:12:49 +0900

    Feat: 상세화면 DB에서 해당 직원 데이터 가져와서 리덕스 state에 저장

--- front/src/screens/Detail/DetailScreen.js
+++ front/src/screens/Detail/DetailScreen.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Button, Box } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import { useDispatch, useSelector } from "react-redux";
 import { modify } from "../../store/modalReducer";
+import { load } from "../../store/memberReducer";
+import { memberRepository } from "../../repositories/member-repository";
 
 const Item = ({ label, content }) => {
   return (
@@ -49,10 +51,13 @@ const Item = ({ label, content }) => {
 function DetailScreen() {
   const params = useParams();
   const dispatch = useDispatch();
-  const memberList = useSelector((state) => state.member);
-  const memberInfo = memberList.filter(
-    (member) => member.id === Number(params.id)
-  )[0];
+  const memberInfo = useSelector((state) => state.member.selectedMemberInfo);
+
+  useEffect(() => {
+    memberRepository.getOne(params.id).then((res) => {
+      dispatch(load({ selectedMemberInfo: res.data }));
+    });
+  }, []);
 
   const handleRemove = () => {
     dispatch(
@@ -77,16 +82,16 @@ function DetailScreen() {
         <h3>직원 정보: {params.id}</h3>
       </div>
       <Box sx={{ width: "100%" }}>
-        <Item label={"이름"} content={memberInfo.name} />
-        <Item label={"핸드폰"} content={memberInfo.phone} />
-        <Item label={"생년월일"} content={memberInfo.birth} />
-        <Item label={"부서"} content={memberInfo.team} />
-        <Item label={"직급"} content={memberInfo.rank} />
-        <Item label={"직책"} content={memberInfo.position} />
-        <Item label={"메일주소"} content={memberInfo.email} />
-        <Item label={"사무실번호"} content={memberInfo.officeNum} />
-        <Item label={"팩스번호"} content={memberInfo.faxNum} />
-        <Item label={"담당업무"} content={memberInfo.task} />
+        <Item label={"이름"} content={memberInfo?.name} />
+        <Item label={"핸드폰"} content={memberInfo?.phone} />
+        <Item label={"생년월일"} content={memberInfo?.birth} />
+        <Item label={"부서"} content={memberInfo?.team} />
+        <Item label={"직급"} content={memberInfo?.rank} />
+        <Item label={"직책"} content={memberInfo?.position} />
+        <Item label={"메일주소"} content={memberInfo?.email} />
+        <Item label={"사무실번호"} content={memberInfo?.officeNum} />
+        <Item label={"팩스번호"} content={memberInfo?.faxNum} />
+        <Item label={"담당업무"} content={memberInfo?.task} />
       </Box>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Link to={"/"} style={{ textDecoration: "none" }}>
