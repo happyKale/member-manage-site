5cfd44ec69c09224f749f4ebac086454b7c0b162
happyKale <syk767@gmail.com>
2022-08-23 23:17:15 +0900

    Style: Header 컴포넌트 높이 수정

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 
-function Header(){
-    return (
-        <div style={{display: 'flex', height: '150px'}}>
-            <h1 style={{alignItems:'center', margin: 'auto', padding: '0px', textAlign:'center'}}>
-                D'Amo 직원 명단 관리 사이트
-            </h1>
-        </div>
-    );
-};
+function Header() {
+  return (
+    <div style={{ display: "flex", height: "100px" }}>
+      <h1
+        style={{
+          alignItems: "center",
+          margin: "auto",
+          padding: "0px",
+          textAlign: "center",
+        }}
+      >
+        D'Amo 직원 명단 관리 사이트
+      </h1>
+    </div>
+  );
+}
 
 export { Header };
