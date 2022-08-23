b71c7881a2cc6ebf63c183f25963e8949fdeacc2
happyKale <syk767@gmail.com>
2022-08-24 00:01:50 +0900

    Edit: 수정화면 직급,직책 상수 값 수정

--- front/src/screens/Modify/ModifyScreen.js
+++ front/src/screens/Modify/ModifyScreen.js
@@ -8,8 +8,29 @@ import { memberRepository } from "../../repositories/member-repository";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const TEAMLIST = ["DA팀", "DE팀", "DK팀", "DP팀", "DX팀"];
-const RANKLIST = ["사원", "대리", "과장", "차장", "부장"]; //직급
-const POSITIONLIST = ["팀원", "팀장"]; //직책
+const RANKLIST = [
+  "인턴",
+  "사원",
+  "대리",
+  "과장",
+  "차장",
+  "부장",
+  "상무",
+  "이사보",
+  "이사",
+  "부사장",
+  "사장",
+]; //직급
+const POSITIONLIST = [
+  "팀원",
+  "팀장",
+  "부원",
+  "실장",
+  "부서장",
+  "본부장",
+  "대표이사(CEO)",
+  "미래위원회 의장",
+]; //직책
 const MARGINBOTTOM = "30px";
 
 function ModifyScreen() {
