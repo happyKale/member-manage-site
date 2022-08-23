7786d93d4331c12aa7ec6025fc665542392d8d66
happyKale <syk767@gmail.com>
2022-08-23 23:16:31 +0900

    Style: 삭제모달 Dialog, DataGrid 컴포넌트 사용해서 재구성

--- front/src/components/RemoveModal/RemoveModal.js
+++ front/src/components/RemoveModal/RemoveModal.js
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
-import { Box, Button, Modal } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { modify } from "../../store/modalReducer";
 import { remove } from "../../store/memberReducer";
 import { useLocation, useNavigate } from "react-router-dom";
+import { memberRepository } from "./../../repositories/member-repository";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import CloseIcon from "@mui/icons-material/Close";
+
+const COLUMNS = [
+  { field: "name", headerName: "사원명", width: 110 },
+  { field: "team", headerName: "부서", width: 100 },
+  { field: "position", headerName: "직책", width: 100 },
+  { field: "rank", headerName: "직급", width: 100 },
+];
 
 function RemoveModal() {
   const navigate = useNavigate();
@@ -11,6 +28,19 @@ function RemoveModal() {
   const dispatch = useDispatch();
   const type = location.pathname.split("/")[1];
   const selectedId = useSelector((state) => state.modal.selectedMemberId);
+  const memberList = useSelector((state) => state.member.memberList);
+  const rows = memberList?.filter((member) => {
+    if (selectedId.includes(member.id)) {
+      return {
+        id: member.id,
+        name: member.name,
+        team: member.team,
+        position: member.position,
+        rank: member.rank,
+      };
+    }
+  });
+
   const [open, setOpen] = useState(true);
 
   const handleClose = () => {
@@ -18,42 +48,62 @@ function RemoveModal() {
     dispatch(modify({ type: "", openStatus: false, selectedMemberId: [] }));
   };
 
-  const handleRemove = async () => {
+  const handleRemove = () => {
     handleClose();
     if (type === "detail") {
       navigate("/");
     }
-    await dispatch(remove({ selectedMemberId: selectedId }));
+    memberRepository.delete({ selectedId: selectedId }).then((res) => {
+      dispatch(remove({ selectedMemberId: selectedId }));
+    });
   };
 
   return (
-    <Modal open={open} onClose={handleClose}>
-      <Box
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle
+        onClose={handleClose}
         style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "400px",
-          height: "400px",
-          backgroundColor: "white",
+          display: "flex",
+          justifyContent: "space-between",
+          borderBottom: "1px solid #e5e5e5",
+          padding: "10px 20px",
+          fontWeight: "bold",
+          color: "gray",
         }}
       >
-        삭제 모달
+        직원 명단 삭제
+        <Button onClick={handleClose}>
+          <CloseIcon />
+        </Button>
+      </DialogTitle>
+      <DialogContent style={{ width: "500px", padding: "20px" }}>
+        <Typography style={{ fontWeight: "bold" }}>
+          선택된 직원 명단 개수: {selectedId.length}
+        </Typography>
+        <Typography style={{ marginTop: "30px" }}>
+          아래 직원 명단을 삭제하시겠습니까?
+        </Typography>
+        <DataGrid
+          headerHeight={40}
+          rowHeight={45}
+          style={{ height: "250px", width: "460px", margin: "20px auto" }}
+          columns={COLUMNS}
+          rows={rows}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+        />
+      </DialogContent>
+      <DialogActions
+        style={{ padding: "10px 20px 10px", borderTop: "1px solid #e5e5e5" }}
+      >
         <Button
-          style={{
-            width: "80px",
-            height: "35px",
-            backgroundColor: "green",
-            marginRight: "15px",
-            color: "white",
-          }}
           onClick={handleRemove}
+          style={{ backgroundColor: "#1386d2", color: "white" }}
         >
           삭제하기
         </Button>
-      </Box>
-    </Modal>
+      </DialogActions>
+    </Dialog>
   );
 }
 
