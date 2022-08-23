d00dfc09d6af2f8aca61fd31495c3e02f711c4ef
happyKale <syk767@gmail.com>
2022-08-24 00:58:07 +0900

    Feat: inputOption 리듀서 생성

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 import memberSlice from "./memberReducer";
 import modalSlice from "./modalReducer";
+import inputOptionSlice from "./inputOptionReducer";
 
 const logger = createLogger();
 
 const reducer = combineReducers({
   member: memberSlice,
   modal: modalSlice,
+  inputOption: inputOptionSlice,
 });
 
 const store = configureStore({
---
+++ front/src/store/inputOptionReducer.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  teamList: ["DA팀", "DE팀", "DK팀", "DP팀", "DX팀"],
+  rankList: [
+    "인턴",
+    "사원",
+    "대리",
+    "과장",
+    "차장",
+    "부장",
+    "상무",
+    "이사보",
+    "이사",
+    "부사장",
+    "사장",
+  ], // 직급
+  positionList: [
+    "팀원",
+    "팀장",
+    "부원",
+    "실장",
+    "부서장",
+    "본부장",
+    "대표이사(CEO)",
+    "미래위원회 의장",
+  ], // 직책
+};
+
+export const inputOptionSlice = createSlice({
+  name: "inputOption",
+  initialState,
+  reducers: {},
+});
+
+export default inputOptionSlice.reducer;
