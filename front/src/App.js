4155ff186f8ba887ca130beb53cac548847ebb58
happyKale <syk767@gmail.com>
2022-08-24 15:12:45 +0900

    Style: Spoqa Han Sans Neo 폰트 적용

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,16 @@
-import { Modal } from "@mui/material";
 import { Container } from "@mui/system";
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import { ConfirmModal, Footer, Header, RemoveModal } from "./components";
+import { ConfirmModal, Header, RemoveModal } from "./components";
 import { HomeScreen, DetailScreen, AddScreen, ModifyScreen } from "./screens";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Spoqa Han Sans Neo",
+  },
+});
 
 function App() {
   const modalInfo = useSelector((state) => state.modal);
@@ -20,18 +26,19 @@ function App() {
         ) : (
           ""
         )}
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="detail">
-            <Route path=":id" element={<DetailScreen />} />
-          </Route>
-          <Route path="add" element={<AddScreen />} />
-          <Route path="modify">
-            <Route path=":id" element={<ModifyScreen />} />
-          </Route>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="detail">
+              <Route path=":id" element={<DetailScreen />} />
+            </Route>
+            <Route path="add" element={<AddScreen />} />
+            <Route path="modify">
+              <Route path=":id" element={<ModifyScreen />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </Container>
-      <Footer />
     </div>
   );
 }
