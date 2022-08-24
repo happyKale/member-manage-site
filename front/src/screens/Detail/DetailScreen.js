df69fe1e0dd97fc380d9d6234f53572c31de9187
happyKale <syk767@gmail.com>
2022-08-24 17:43:54 +0900

    Style: 상세화면 Stack 컴포넌트 사용해서 구조 재구성

--- front/src/screens/Detail/DetailScreen.js
+++ front/src/screens/Detail/DetailScreen.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Box } from "@mui/material";
-import Stack from "@mui/material/Stack";
 import { useDispatch, useSelector } from "react-redux";
 import { modify } from "../../store/modalReducer";
 import { load } from "../../store/memberReducer";
 import { memberRepository } from "../../repositories/member-repository";
+import { Button, Box, Stack, Typography, Divider } from "@mui/material";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import SquareIcon from "@mui/icons-material/Square";
 
 const Item = ({ label, content }) => {
   return (
@@ -13,15 +14,15 @@ const Item = ({ label, content }) => {
       direction="row"
       spacing={2}
       style={{
-        border: "1px solid lightgrey",
+        border: "1px solid #eef2f5",
         height: "50px",
         marginBottom: "10px",
-        borderRadius: "4px",
+        borderRadius: "2px",
       }}
     >
       <span
         style={{
-          backgroundColor: "lightgrey",
+          backgroundColor: "#eef2f5",
           display: "inline-block",
           width: "25%",
           borderTopLeftRadius: "4px",
@@ -39,7 +40,7 @@ const Item = ({ label, content }) => {
           borderTopRightRadius: "4px",
           borderBottomRightRadius: "4px",
           lineHeight: "45px",
-          paddingLeft: "15px",
+          paddingLeft: "10px",
         }}
       >
         {content}
@@ -78,21 +79,92 @@ function DetailScreen() {
         padding: "50px 0",
       }}
     >
-      <div>
-        <h3>직원 정보: {params.id}</h3>
-      </div>
-      <Box sx={{ width: "100%" }}>
-        <Item label={"이름"} content={memberInfo?.name} />
-        <Item label={"핸드폰"} content={memberInfo?.phone} />
-        <Item label={"생년월일"} content={memberInfo?.birth} />
-        <Item label={"부서"} content={memberInfo?.team} />
-        <Item label={"직급"} content={memberInfo?.rank} />
-        <Item label={"직책"} content={memberInfo?.position} />
-        <Item label={"메일주소"} content={memberInfo?.email} />
-        <Item label={"사무실번호"} content={memberInfo?.officeNum} />
-        <Item label={"팩스번호"} content={memberInfo?.faxNum} />
-        <Item label={"담당업무"} content={memberInfo?.task} />
-      </Box>
+      <Typography style={{ fontSize: "24px", fontWeight: "bold" }}>
+        직원 정보
+      </Typography>
+      <Stack
+        direction={"column"}
+        style={{
+          borderTop: "1px solid #e5e5e5",
+          borderBottom: "1px solid #e5e5e5",
+          margin: "30px 0",
+        }}
+        divider={<Divider orientation="horizontal" flexItem />}
+      >
+        <Stack direction={"row"} margin={"20px 0"}>
+          <Typography
+            style={{
+              width: "25%",
+              fontSize: "18px",
+              fontWeight: "bold",
+              color: "#5b5b5b",
+            }}
+          >
+            <SquareIcon
+              style={{
+                fontSize: "12px",
+                marginRight: "5px",
+                paddingBottom: "3px",
+              }}
+            />
+            기본 직원 정보
+          </Typography>
+          <Stack style={{ width: "75%" }}>
+            <Item label={"이름"} content={memberInfo?.name} />
+            <Item label={"부서"} content={memberInfo?.team} />
+            <Item label={"직급"} content={memberInfo?.rank} />
+            <Item label={"직책"} content={memberInfo?.position} />
+          </Stack>
+        </Stack>
+        <Stack direction={"row"} margin={"20px 0"}>
+          <Typography
+            style={{
+              width: "25%",
+              fontSize: "18px",
+              fontWeight: "bold",
+              color: "#5b5b5b",
+            }}
+          >
+            <SquareIcon
+              style={{
+                fontSize: "12px",
+                marginRight: "5px",
+                paddingBottom: "3px",
+              }}
+            />
+            직원 연락처
+          </Typography>
+          <Stack style={{ width: "75%" }}>
+            <Item label={"핸드폰"} content={memberInfo?.phone} />
+            <Item label={"메일주소"} content={memberInfo?.email} />
+            <Item label={"사무실번호"} content={memberInfo?.officeNum} />
+            <Item label={"팩스번호"} content={memberInfo?.faxNum} />
+          </Stack>
+        </Stack>
+        <Stack direction={"row"} margin={"20px 0"}>
+          <Typography
+            style={{
+              width: "25%",
+              fontSize: "18px",
+              fontWeight: "bold",
+              color: "#5b5b5b",
+            }}
+          >
+            <SquareIcon
+              style={{
+                fontSize: "12px",
+                marginRight: "5px",
+                paddingBottom: "3px",
+              }}
+            />
+            기타 직원 정보
+          </Typography>
+          <Stack style={{ width: "75%" }}>
+            <Item label={"생년월일"} content={memberInfo?.birth} />
+            <Item label={"담당업무"} content={memberInfo?.task} />
+          </Stack>
+        </Stack>
+      </Stack>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Link to={"/"} style={{ textDecoration: "none" }}>
           <Button
@@ -100,10 +172,14 @@ function DetailScreen() {
             style={{
               width: "80px",
               height: "35px",
-              backgroundColor: "lightgray",
-              color: "white",
+              backgroundColor: "white",
+              color: "gray",
+              border: "1.5px solid #d3d3d3",
+              padding: "1.5px",
+              fontWeight: "bold",
             }}
           >
+            <ArrowBackIosIcon fontSize="14px" />
             목록
           </Button>
         </Link>
@@ -114,9 +190,10 @@ function DetailScreen() {
               style={{
                 width: "80px",
                 height: "35px",
-                backgroundColor: "blue",
+                backgroundColor: "#1386d2",
                 marginRight: "15px",
                 color: "white",
+                fontWeight: "bold",
               }}
             >
               수정
@@ -127,8 +204,10 @@ function DetailScreen() {
             style={{
               width: "80px",
               height: "35px",
-              backgroundColor: "red",
-              color: "white",
+              backgroundColor: "white",
+              color: "gray",
+              border: "1.5px solid #d3d3d3",
+              fontWeight: "bold",
             }}
             onClick={handleRemove}
           >
