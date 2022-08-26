5f09be03fa4671f444164079c0c03f73f2fc78c9
happyKale <syk767@gmail.com>
2022-08-26 16:59:12 +0900

    Feat: 핸드폰 번호가 유효한지 체크하는 함수 checkPhoneNumber 추가

---
+++ front/src/libs/common/index.js
@@ -0,0 +1,20 @@
+// 정규식으로 변경하기!!
+// 핸드폰 번호 규격에 맞는 값인지 확인.
+export const checkPhoneNumber = (phone) => {
+  if (phone !== "") {
+    const isNumber = phone
+      .split("-")
+      .every((num) => String(Number(num)) !== "NaN");
+    if (!isNumber) {
+      return {
+        status: false,
+        message: "핸드폰 번호는 숫자만 입력할 수 있습니다.",
+      };
+    }
+    if (phone.length !== 0 && phone.length !== 13) {
+      return { status: false, message: "핸드폰 번호를 전부 입력해주세요." };
+    }
+  } else {
+    return { status: true, message: "핸드폰 번호가 입력되지 않았습니다." };
+  }
+};
