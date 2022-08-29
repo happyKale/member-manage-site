131649afbc1c570fecdb75e8e3237a936277d559
happyKale <syk767@gmail.com>
2022-08-30 00:49:07 +0900

    Edit: dataGridColumnData의 home 사원명 필드의 너비 수정

--- front/src/asset/dataGridColumnData.js
+++ front/src/asset/dataGridColumnData.js
@@ -3,7 +3,7 @@ export const dataGridColumnData = {
     { field: "team", headerName: "부서", width: 110 },
     { field: "rank", headerName: "직급", width: 110 },
     { field: "position", headerName: "직책", width: 110 },
-    { field: "name", headerName: "사원명", width: 110 },
+    { field: "name", headerName: "사원명", width: 140 },
     { field: "officeNum", headerName: "사무실번호", width: 160 },
     { field: "phone", headerName: "핸드폰", width: 160 },
   ],
