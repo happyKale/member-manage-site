02c4398f8415f92fb82962dc6d34a5315fab12fa
happyKale <syk767@gmail.com>
2022-08-29 18:03:15 +0900

    Style: 메인화면 muiStyles.js 파일에 globalMuiStyles.js파일 적용

--- front/src/screens/Home/muiStyles.js
+++ front/src/screens/Home/muiStyles.js
@@ -1,29 +1,9 @@
+import { globalMuiStyles } from "../../styles/globalMuiStyles";
+
 export const styles = {
+  ...globalMuiStyles,
   container: {
     padding: "30px 0",
-  },
-  link: {
-    textDecoration: "none",
-  },
-  btn: {
-    width: "80px",
-    height: "35px",
-    fontWeight: "bold",
-    backgroundColor: "white",
-    color: "gray",
-    border: "1.5px solid #d3d3d3",
-  },
-  btnPositive: {
-    width: "80px",
-    height: "35px",
-    fontWeight: "bold",
-    backgroundColor: "#1386d2",
-    color: "white",
-    marginRight: "15px",
-    border: "none",
-    "&:hover": {
-      backgroundColor: "#4792d8",
-    },
   },
   boxDataGrid: {
     height: "520px",
