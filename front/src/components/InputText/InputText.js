ee051aecce4809e8bbbc7b4f02e976806419eb14
happyKale <syk767@gmail.com>
2022-08-29 11:14:23 +0900

    Feat: InputText 컴포넌트 생성

---
+++ front/src/components/InputText/InputText.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { TextField, InputLabel, Box } from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
+
+const helperTextStyles = makeStyles((theme) => ({
+  root: {
+    color: "white",
+    margin: "5px 0 0 0",
+  },
+  error: {
+    "&.MuiFormHelperText-root.Mui-error": {
+      color: "red",
+      margin: "5px 0 0 0",
+    },
+  },
+}));
+
+function InputText({
+  label,
+  name,
+  placeholder,
+  inputProps,
+  required,
+  requiredCheck,
+  onChange,
+  inputMemberInfo,
+  ariaDescribedby,
+  helperText,
+  marginBottom,
+}) {
+  const helperTestClasses = helperTextStyles();
+
+  return (
+    <Box style={{ marginBottom: `${marginBottom}` }}>
+      <InputLabel style={{ margin: "0 0 5px 0" }}>
+        {label}
+        {required && <span style={{ color: "red" }}> *</span>}
+      </InputLabel>
+      <TextField
+        fullWidth
+        error={requiredCheck[name] ? false : true}
+        aria-describedby={ariaDescribedby}
+        value={inputMemberInfo[name]}
+        name={name}
+        onChange={onChange}
+        placeholder={placeholder ?? ""}
+        inputProps={inputProps}
+        helperText={helperText ?? ""}
+        FormHelperTextProps={{
+          classes: helperTestClasses,
+        }}
+      />
+    </Box>
+  );
+}
+
+export default InputText;
--- front/src/components/index.js
+++ front/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
 export { default as RemoveModal } from "./RemoveModal/RemoveModal";
 export { default as ConfirmModal } from "./ConfirmModal/ConfirmModal";
+export { default as InputText } from "./InputText/InputText";
