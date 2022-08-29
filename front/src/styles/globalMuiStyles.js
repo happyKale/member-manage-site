9335a9ae2f915a0f21a071a69ff61fda1a9bf64a
happyKale <syk767@gmail.com>
2022-08-30 00:45:19 +0900

    Edit: globlaMuiStyles 속성 추가

--- front/src/styles/globalMuiStyles.js
+++ front/src/styles/globalMuiStyles.js
@@ -1,13 +1,48 @@
 export const globalMuiStyles = {
+  marginBottom: {
+    marginBottom: "20px",
+  },
   // 화면 소제목
   screenTitle: {
     fontSize: "20px",
     fontWeight: "bold",
   },
+  screenContent: {
+    borderTop: "1px solid #e5e5e5",
+    borderBottom: "1px solid #e5e5e5",
+    margin: "30px 0",
+  },
+
+  // input
+  inputLabel: {
+    margin: "0 0 5px 0",
+  },
+  inputContainer: {
+    width: "65%",
+  },
+
+  //section
+  section: {
+    margin: "20px 0",
+  },
+  sectionTitle: {
+    width: "35%",
+    fontSize: "18px",
+    fontWeight: "bold",
+    color: "#5b5b5b",
+  },
 
   // 버튼
-  btn: {
+  btnSmall: {
     width: "80px",
+    height: "35px",
+    fontWeight: "bold",
+    backgroundColor: "white",
+    color: "gray",
+    border: "1.5px solid #d3d3d3",
+  },
+  btnLarge: {
+    width: "110px",
     height: "35px",
     fontWeight: "bold",
     backgroundColor: "white",
@@ -30,8 +65,13 @@ export const globalMuiStyles = {
   },
 
   // 아이콘
-  iconSquare: {
+  iconSquareSmall: {
     fontSize: "10px",
+    marginRight: "5px",
+    paddingBottom: "3px",
+  },
+  iconSquareLarge: {
+    fontSize: "12px",
     marginRight: "5px",
     paddingBottom: "3px",
   },
