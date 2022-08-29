5a289cf9434ae427a79828d54c88675baf88060a
happyKale <syk767@gmail.com>
2022-08-29 17:11:07 +0900

    Feat: DataGrid 컴포넌트에 사용되는 컬럼 값을 asset폴더에 파일로 분리

---
+++ front/src/asset/dataGridColumnData.js
@@ -0,0 +1,16 @@
+export const dataGridColumnData = {
+  home: [
+    { field: "team", headerName: "부서", width: 110 },
+    { field: "rank", headerName: "직급", width: 110 },
+    { field: "position", headerName: "직책", width: 110 },
+    { field: "name", headerName: "사원명", width: 110 },
+    { field: "officeNum", headerName: "사무실번호", width: 160 },
+    { field: "phone", headerName: "핸드폰", width: 160 },
+  ],
+  remove: [
+    { field: "name", headerName: "사원명", width: 110 },
+    { field: "team", headerName: "부서", width: 100 },
+    { field: "position", headerName: "직책", width: 100 },
+    { field: "rank", headerName: "직급", width: 100 },
+  ],
+};
