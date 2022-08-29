19341ad4afca86756537e2eb03b169873eb3c056
happyKale <syk767@gmail.com>
2022-08-29 18:01:48 +0900

    Style: 공통적으로 적용되는 mui 스타일을 globalMuiStyles.js파일로 생성

---
+++ front/src/styles/globalMuiStyles.js
@@ -0,0 +1,41 @@
+export const globalMuiStyles = {
+  // 화면 소제목
+  screenTitle: {
+    fontSize: "20px",
+    fontWeight: "bold",
+  },
+
+  // 버튼
+  btn: {
+    width: "80px",
+    height: "35px",
+    fontWeight: "bold",
+    backgroundColor: "white",
+    color: "gray",
+    border: "1.5px solid #d3d3d3",
+  },
+  btnPositive: {
+    width: "80px",
+    height: "35px",
+    fontWeight: "bold",
+    backgroundColor: "#1386d2",
+    color: "white",
+    border: "none",
+    "&:hover": {
+      backgroundColor: "#4792d8",
+    },
+  },
+  btnMargin: {
+    marginRight: "15px",
+  },
+
+  // 아이콘
+  iconSquare: {
+    fontSize: "10px",
+    marginRight: "5px",
+    paddingBottom: "3px",
+  },
+  iconArrow: {
+    fontSize: "14px",
+  },
+};
