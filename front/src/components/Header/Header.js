d808dae269c5c9783dcb4241a943c1f858eea4a1
happyKale <syk767@gmail.com>
2022-08-30 15:17:41 +0900

    Refactor: Header 컴포넌트 muiStyles 적용

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Stack, Typography } from "@mui/material";
+import { styles as muiStyles } from "./muiStyles";
 
 function Header() {
   return (
-    <div style={{ display: "flex", height: "80px" }}>
-      <h1
-        style={{
-          alignItems: "center",
-          margin: "auto",
-          padding: "0px",
-          textAlign: "center",
-        }}
-      >
+    <Stack sx={muiStyles.container}>
+      <Typography sx={muiStyles.title}>
         <Link to={"/"} style={{ color: "black" }}>
           D'Amo 직원 명단 관리 사이트
         </Link>
-      </h1>
-    </div>
+      </Typography>
+    </Stack>
   );
 }
 
---
+++ front/src/components/Header/muiStyles.js
@@ -0,0 +1,17 @@
+import { globalMuiStyles } from "../../styles/globalMuiStyles";
+
+export const styles = {
+  ...globalMuiStyles,
+  container: {
+    flexDirection: "row",
+    height: "80px",
+  },
+  title: {
+    alignItems: "center",
+    margin: "auto",
+    padding: "0px",
+    textAlign: "center",
+    fontSize: "32px",
+    fontWeight: "bold",
+  },
+};
