929fb0039b61da0158dac33787374de66847053e
happyKale <syk767@gmail.com>
2022-08-30 15:20:30 +0900

    Refactor: 삭제모달 css styles, muiStyles 적용

--- front/src/components/RemoveModal/RemoveModal.js
+++ front/src/components/RemoveModal/RemoveModal.js
@@ -4,6 +4,7 @@ import { modify } from "../../store/modalReducer";
 import { modifySelectedIdList, remove } from "../../store/memberReducer";
 import { useLocation, useNavigate } from "react-router-dom";
 import { memberRepository } from "./../../repositories/member-repository";
+import { dataGridColumnData } from "./../../asset/dataGridColumnData";
 import {
   Button,
   Dialog,
@@ -14,7 +15,7 @@ import {
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import CloseIcon from "@mui/icons-material/Close";
-import { dataGridColumnData } from "./../../asset/dataGridColumnData";
+import { styles as muiStyles } from "./muiStyles";
 
 function RemoveModal() {
   const navigate = useNavigate();
@@ -59,66 +60,38 @@ function RemoveModal() {
 
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle
-        onClose={handleClose}
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          borderBottom: "1px solid #e5e5e5",
-          padding: "10px 20px",
-          fontWeight: "bold",
-          color: "gray",
-        }}
-      >
+      <DialogTitle onClose={handleClose} sx={muiStyles.dialogTitle}>
         직원 명단 삭제
         <Button onClick={handleClose}>
           <CloseIcon />
         </Button>
       </DialogTitle>
-      <DialogContent style={{ width: "500px", padding: "20px" }}>
-        <Typography style={{ fontWeight: "bold" }}>
+      <DialogContent sx={muiStyles.dialogContent}>
+        <Typography sx={muiStyles.dialogContentTitle}>
           선택된 직원 명단 개수: {selectedIdList?.length}
         </Typography>
-        <Typography style={{ marginTop: "30px" }}>
+        <Typography sx={muiStyles.dialogContentText}>
           아래 직원 명단을 삭제하시겠습니까?
         </Typography>
         <DataGrid
           headerHeight={40}
           rowHeight={45}
-          style={{ height: "250px", width: "460px", margin: "20px auto" }}
+          sx={muiStyles.dialogDataGrid}
           columns={COLUMNS}
           rows={memberList.length === 0 ? [selectedMemberInfo] : rows}
           pageSize={5}
           rowsPerPageOptions={[5]}
         />
       </DialogContent>
-      <DialogActions
-        style={{ padding: "10px 20px 10px", borderTop: "1px solid #e5e5e5" }}
-      >
+      <DialogActions sx={muiStyles.dialogActions}>
         <Button
           onClick={handleRemove}
           variant="text"
-          style={{
-            width: "80px",
-            height: "35px",
-            backgroundColor: "#1386d2",
-            color: "white",
-          }}
+          sx={muiStyles.btnPositive}
         >
           삭제하기
         </Button>
-        <Button
-          onClick={handleClose}
-          variant="text"
-          style={{
-            width: "80px",
-            height: "35px",
-            backgroundColor: "white",
-            color: "gray",
-            border: "1.5px solid #d3d3d3",
-            fontWeight: "bold",
-          }}
-        >
+        <Button onClick={handleClose} variant="text" sx={muiStyles.btnSmall}>
           취소하기
         </Button>
       </DialogActions>
---
+++ front/src/components/RemoveModal/muiStyles.js
@@ -0,0 +1,18 @@
+import { globalMuiStyles } from "../../styles/globalMuiStyles";
+
+export const styles = {
+  ...globalMuiStyles,
+  dialogContent: {
+    width: "500px",
+    padding: "20px",
+    marginTop: "20px",
+  },
+  dialogDataGrid: {
+    height: "250px",
+    width: "460px",
+    margin: "20px auto",
+  },
+  dialogContentText: {
+    marginTop: "30px",
+  },
+};
