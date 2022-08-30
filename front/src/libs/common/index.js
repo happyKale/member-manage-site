c63e7b51dfab6075e996ba585a1a158d603b7c8e
happyKale <syk767@gmail.com>
2022-08-30 11:03:55 +0900

    Fix: 핸드폰 번호 확인 함수 값이 있는 경우 return 값 추가

--- front/src/libs/common/index.js
+++ front/src/libs/common/index.js
@@ -14,6 +14,7 @@ export const checkPhoneNumber = (phone) => {
     if (phone.length !== 0 && phone.length !== 13) {
       return { status: false, message: "핸드폰 번호를 전부 입력해주세요." };
     }
+    return { status: true, message: "올바른 핸드폰 번호 입니다." };
   } else {
     return { status: true, message: "핸드폰 번호가 입력되지 않았습니다." };
   }
